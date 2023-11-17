deefc944579e569a6d543612784d32c332b85354
didierrc <didieryamil02@gmail.com>
2023-11-17 19:18:46 +0100

    Finished Meteo API exercise

--- Escritorio-Virtual/js/pais.js
+++ Escritorio-Virtual/js/pais.js
@@ -1,7 +1,6 @@
 // Key: 5b4e304cb2042d8fdd3b883201af3d82
 // Example of call: api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=5b4e304cb2042d8fdd3b883201af3d82
 // API documentation: https://openweathermap.org/forecast5
-
 "use strict";
 class Pais {
 
@@ -99,7 +98,6 @@ class Pais {
                         break;
                 }
 
-                console.log(forecast5Days);
                 this.insertaMeteoEnHtml(forecast5Days);
             },
             error: (e) => {
@@ -113,39 +111,86 @@ class Pais {
 
     insertaMeteoEnHtml(forecasts) {
 
-        for (var i = 0; i < forecasts.length; i++) {
+        var insertaSeccionInfo = (imageUrl, imageAlt, t) => {
+            $("main article:last-child>section:last-child").append("<section></section>");
 
-            var forecast = forecasts[i];
+            $("main article:last-child section:last-child>section:last-child").append("<img>");
+            $("main article:last-child section:last-child>section:last-child>img").attr("src", imageUrl);
+            $("main article:last-child section:last-child>section:last-child>img").attr("alt", imageAlt);
+            $("main article:last-child section:last-child>section:last-child").append(t);
+        }
 
-            var tempMax = "<li> Temp. max: " + forecast["main"]["temp_max"] + "</li>";
-            var tempMin = "<li> Temp. min:" + forecast["main"]["temp_min"] + "</li>";
-            var humidityPercentage = "<li> Humidity Percentage: " + forecast["main"]["humidity"] + "</li>";
-            var icon = "<li>" + "Icon:" + i + "</li>";
+        var insertaTituloForecast = (forecast) => {
+            var forecastDate = new Date(forecast["dt_txt"]);
+            var weekday = forecastDate.toLocaleDateString("es-ES", { weekday: "long" });
+            weekday = weekday.charAt(0).toUpperCase() + weekday.slice(1); // Solo para capitalizar la primera letra
+            var hour = forecastDate.getHours() + ":" + forecastDate.getMinutes() + "0";
+            var title = "<p>" + weekday + " " + hour + "</p>";
 
-            // There may not rain
-            var rainAmount;
-            try {
-                rainAmount = "<li>Rain: " + forecast["rain"]["3h"] + "</li>";
-            } catch (error) {
-                rainAmount = "";
-            }
+            $("main article:last-child>section:last-child").append(title);
+        }
 
+        var insertaImagenForecast = (forecast) => {
+            var imageUrl = "http://openweathermap.org/img/w/"
+                + forecast["weather"][0]["icon"] + ".png";
+            var imageAlt = "Representación de: " + forecast["weather"][0]["description"];
 
-
-            var testList = "<ul>" + tempMax + tempMin + humidityPercentage + icon + rainAmount + "</ul>"
-
-            $("main article:last-child")
-                .append("<p>Forecast for Day: " + forecast["dt_txt"] + "</p>")
-                .append("<ul>" + testList + "</ul>");
-
-
-
-
+            $("main article:last-child>section:last-child").append("<img>");
+            $("main article:last-child>section:last-child>img").attr("src", imageUrl);
+            $("main article:last-child>section:last-child>img").attr("alt", imageAlt);
         }
 
 
-    }
+        for (var i = 0; i < forecasts.length; i++) {
+            var forecast = forecasts[i];
 
+            // Añadiendo la seccion del forecast
+            $("main article:last-child").append("<section></section>");
+
+            // Añadiendo el titulo
+            insertaTituloForecast(forecast);
+
+            // Añadiendo la imagen
+            insertaImagenForecast(forecast);
+
+            // Añadiendo seccion de temperatura maxima
+            var t = "<p>Max: " + forecast["main"]["temp_max"] + "º</p>";
+            var imageUrl = "multimedia/maxTemp_icon.png";
+            var imageAlt = "Icono de temperatura maxima";
+            insertaSeccionInfo(imageUrl, imageAlt, t);
+
+            // Añadiendo seccion de temperatura minima
+            t = "<p>Max: " + forecast["main"]["temp_min"] + "º</p>";
+            imageUrl = "multimedia/minTemp_icon.png";
+            imageAlt = "Icono de temperatura minima";
+            insertaSeccionInfo(imageUrl, imageAlt, t);
+
+            // Añadiendo seccion de lluvia
+            // Puede que no haya la propiedad lluvia en el JSON
+            var rainAmount;
+            try {
+                rainAmount = parseFloat(forecast["rain"]["3h"]) * 100;
+            } catch (error) {
+                rainAmount = "";
+            }
+            t = "<p>Probabilidad de Lluvia: " + (rainAmount === "" ? "0" : rainAmount) + "%";
+            imageUrl = "multimedia/rain_icon.png";
+            imageAlt = "Icono de  probabilidad de lluvia";
+            insertaSeccionInfo(imageUrl, imageAlt, t);
+
+            // Añadiendo seccion de humedad
+            t = "<p>Humedad:" + forecast["main"]["humidity"] + "%</p>";
+            imageUrl = "multimedia/humidity_icon.png";
+            imageAlt = "Icono de humedad en ambiente";
+            insertaSeccionInfo(imageUrl, imageAlt, t);
+
+            // Añadiendo seccion de viento
+            t = "<p>Viento:" + forecast["wind"]["speed"] + "km/h</p>";
+            imageUrl = "multimedia/wind_icon.png";
+            imageAlt = "Icono de viento";
+            insertaSeccionInfo(imageUrl, imageAlt, t);
+        }
+    }
 }
 
 var monaco = new Pais("Monaco", "Monaco", "36 686");
