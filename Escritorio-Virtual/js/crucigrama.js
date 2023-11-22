405f30898577081a3a5cc360a61f7538536da81a
didierrc <didieryamil02@gmail.com>
2023-11-22 19:45:18 +0100

    Finished Crucigrama

--- Escritorio-Virtual/js/crucigrama.js
+++ Escritorio-Virtual/js/crucigrama.js
@@ -14,7 +14,7 @@
 class Crucigrama {
 
     constructor() {
-        this.board = "4,*,.,=,12,#,#,#,5,#,#,*,#,/,#,#,#,*,4,-,.,=,.,#,15,#,.,*,#,=,#,=,#,/,#,=,.,#,3,#,4,*,.,=,20,=,#,#,#,#,#,=,#,#,8,#,9,-,.,=,3,#,.,#,#,-,#,+,#,#,#,*,6,/,.,=,.,#,#,#,.,#,#,=,#,=,#,#,#,=,#,#,6,#,8,*,.,=,16";
+        this.board = "4,.,.,=,36,#,#,#,25,#,#,*,#,.,#,#,#,.,.,-,.,=,.,#,15,#,.,*,#,=,#,=,#,.,#,=,.,#,18,#,6,*,.,=,30,=,#,#,#,#,#,=,#,#,56,#,9,-,.,=,3,#,.,#,#,*,#,+,#,#,#,*,20,.,.,=,18,#,#,#,.,#,#,=,#,=,#,#,#,=,#,#,18,#,24,.,.,=,72";
 
         // Inicializando el array bidimensional
         this.cols = 9;
@@ -25,6 +25,9 @@ class Crucigrama {
 
         this.init_time = null;
         this.end_time = null;
+
+        this.isACellClicked = false;
+        this.cellClicked = null;
 
         this.start();
     }
@@ -62,17 +65,53 @@ class Crucigrama {
 
                 if (valorCelda === 0) {
                     $("main").append("<p></p>");
+                    $("main>p:last").attr("data-i", i);
+                    $("main>p:last").attr("data-j", j);
                 } else if (valorCelda === -1) {
                     $("main").append("<p></p>");
-                    $("main>p:last-child").attr("data-state", "empty");
+                    $("main>p:last").attr("data-state", "empty");
                 } else {
                     $("main").append("<p>" + valorCelda + "</p>");
-                    $("main>p:last-child").attr("data-state", "blocked");
+                    $("main>p:last").attr("data-state", "blocked");
                 }
+
+
+
             }
         }
+
+        // Añadiendo el evento click a todos los p que no tienen el atributo data-state en
+        // el momento de la inicializacion
+        this.addClickEventHandler();
+
         // Empieza el juego
         this.init_time = new Date();
+    }
+
+
+    addClickEventHandler() {
+
+        var pClickables = $("main>p:not([data-state])");
+
+        var i = 0;
+        for (i; i < pClickables.length; i++)
+            pClickables[i].onclick = this.clickCell.bind(pClickables[i], this);
+
+    }
+
+    clickCell(game) {
+
+        if (game.isACellClicked) {
+            if (game.cellClicked != this) {
+                game.cellClicked.removeAttribute('data-state');
+            }
+        }
+
+        this.setAttribute('data-state', "clicked");
+        game.isACellClicked = true;
+        game.cellClicked = this;
+
+
     }
 
     check_win_condition() {
@@ -105,6 +144,156 @@ class Crucigrama {
 
         return hoursElapsed + ":" + minutesElapsed + ":" + secondsElapsed;
     }
+
+    introduceElement(element) {
+        var expression_row = true;
+        var expression_col = true;
+
+        var iOfCell = $(this.cellClicked).attr("data-i");
+        var jOfCell = $(this.cellClicked).attr("data-j");
+        this.crucigrama[iOfCell][jOfCell] = element;
+
+        // Comprobando la horizontal
+
+        var first_number = 0;
+        var second_number = 0;
+        var expression = 0;
+        var result = 0;
+
+        // Partimos de la casilla inmediatamente a la derecha
+        jOfCell++;
+        if (jOfCell < this.cols) { // Comprobamos que esta dentro de los limites
+
+            if (this.crucigrama[iOfCell][jOfCell] != -1) { // Comprobamos que no sea un -1
+                do { // Comprobamos que la casilla es un "="
+
+                    if (this.crucigrama[iOfCell][jOfCell] === "=") { // Si lo es, seteamos los valores
+                        first_number = this.crucigrama[iOfCell][jOfCell - 3];
+                        second_number = this.crucigrama[iOfCell][jOfCell - 1];
+                        expression = this.crucigrama[iOfCell][jOfCell - 2];
+                        result = this.crucigrama[iOfCell][jOfCell + 1];
+                        break; // salimos del bucle
+                    }
+
+                    jOfCell++; // Si no es el elemento que buscamos, aumentamos la j
+
+                    if (jOfCell >= this.cols)
+                        break; // salimos si j is out of bounds 
+
+                } while (this.crucigrama[iOfCell][jOfCell] != -1) // comprobamos que no sea -1
+            }
+
+        }
+
+        // Si los valores son distinto de cero
+        if (first_number != 0 && second_number != 0 && expression != 0 && result != 0) {
+
+            // SE DEBE MIRAR SI LA POSICION DE UN EXPR PUEDE LLEVAR UN NUMERO?
+
+            // Los unimos con join
+            var myExprArray = [first_number, expression, second_number];
+            // Intentamos evaluar la expr (si se introduce una expr en el sitio de un numero,
+            // se captura el error y exp_col = false)
+            var evalResult;
+            try {
+                evalResult = eval(myExprArray.join(''));
+
+                // Comprobamos si el resultado es correto
+                if (evalResult != result)
+                    expression_row = false;
+
+            } catch (error) {
+                expression_row = false;
+            }
+
+        }
+
+        // Comprobando la vertical
+
+        var iOfCell = $(this.cellClicked).attr("data-i");
+        var jOfCell = $(this.cellClicked).attr("data-j");
+
+        var first_number = 0;
+        var second_number = 0;
+        var expression = 0;
+        var result = 0;
+
+        // Partimos de la casilla inmediatamente inferior
+        iOfCell++;
+        if (iOfCell < this.rows) { // Comprobamos que esta dentro de los limites
+            if (this.crucigrama[iOfCell][jOfCell] != -1) {
+                do { // Comprobamos que la casilla es un "="
+
+                    if (this.crucigrama[iOfCell][jOfCell] === "=") { // Si lo es, seteamos los valores
+                        first_number = this.crucigrama[iOfCell - 3][jOfCell];
+                        second_number = this.crucigrama[iOfCell - 1][jOfCell];
+                        expression = this.crucigrama[iOfCell - 2][jOfCell];
+                        result = this.crucigrama[iOfCell + 1][jOfCell];
+                        break; // salimos del bucle
+                    }
+
+                    iOfCell++; // Si no es el elemento que buscamos, aumentamos la i
+
+                    if (iOfCell >= this.rows)
+                        break; // salimos si i is out of bounds 
+
+                } while (this.crucigrama[iOfCell][jOfCell] != -1) // comprobamos que no sea -1
+
+            }
+        }
+
+        // Si los valores son distinto de cero
+        if (first_number != 0 && second_number != 0 && expression != 0 && result != 0) {
+
+            // Los unimos con join
+            var myExprArray = [first_number, expression, second_number];
+            // Intentamos evaluar la expr (si se introduce una expr en el sitio de un numero,
+            // se captura el error y exp_col = false)
+            var evalResult;
+            try {
+                evalResult = eval(myExprArray.join(''));
+
+                // Comprobamos si el resultado es correto
+                if (evalResult != result)
+                    expression_col = false;
+
+            } catch (error) {
+                expression_col = false;
+            }
+
+
+
+        }
+
+        // Mostramos el elemento en pantalla o no
+
+        var iOfCell = $(this.cellClicked).attr("data-i");
+        var jOfCell = $(this.cellClicked).attr("data-j");
+
+        if (expression_row && expression_col) {
+
+            $(this.cellClicked).text(element); // Mostramos el valor introducido
+            $(this.cellClicked).attr("data-state", "correct"); // Estado en correcto
+            // QUITAMOS EL EVENTO ONCLICK?
+        } else {
+            this.crucigrama[iOfCell][jOfCell] = 0;
+            $(this.cellClicked).removeAttr("data-state"); // deja de estar seleccionada
+            alert("Elemento introducido no correcto");
+        }
+
+        // Se deselecciona la celda
+        this.isACellClicked = false;
+        this.cellClicked = null;
+
+        // Comprobamos el final del crucigrama
+        if (this.check_win_condition()) {
+            this.end_time = new Date();
+            alert("El juego ha terminado!!. Duración: " + this.calculate_date_difference());
+        }
+
+
+    }
+
 }
 
 var juego = new Crucigrama();
